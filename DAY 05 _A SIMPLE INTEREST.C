2f388eff7e7421e8c732326ea2943befcda5d270
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-01 22:17:50 +0530

    Update and rename day 08 count of numbers.c to DAY 05 _A SIMPLE INTEREST.C

---
+++ DAY 05 _A SIMPLE INTEREST.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main() {
+    float principal, rate, time;
+    float simpleInterest, compoundInterest;
+
+
+    printf("Enter Principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter Rate of interest: ");
+    scanf("%f", &rate);
+
+    printf("Enter Time (in years): ");
+    scanf("%f", &time);
+
+    
+    simpleInterest = (principal * rate * time) / 100;
+
+    
+    compoundInterest = principal * pow((1 + rate / 100), time) - principal;
+
+    
+    printf("\nSimple Interest = %.2f", simpleInterest);
+    printf("\nCompound Interest = %.2f", compoundInterest);
+
+    return 0;
+}
+
