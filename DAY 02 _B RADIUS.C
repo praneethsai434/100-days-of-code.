47bb68d6d86900fca8086ba108327d4d419d7ca6
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-01 22:07:42 +0530

    Update and rename day 03 even or odd .c to DAY 02 _B RADIUS.C

---
+++ DAY 02 _B RADIUS.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define PI 3.14159   
+
+int main() {
+    float radius, area, circumference;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+
+    printf("\nArea of the circle = %.2f", area);
+    printf("\nCircumference of the circle = %.2f", circumference);
+
+    return 0;
+}
