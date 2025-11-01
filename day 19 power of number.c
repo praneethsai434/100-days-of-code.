69501250c9fd17026b3e82d8902bcaa8f460de00
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-01 22:40:03 +0530

    Update and rename day 19 power of number.c to DAY 10 _ B .C

--- day 19 power of number.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int m = 2, n = 5, result = 1;
-    for (int i = 1; i <= n; i++) {
-        result *= m;
-    }
-    printf("%d^%d = %d", m, n, result);
-    return 0;
-}
