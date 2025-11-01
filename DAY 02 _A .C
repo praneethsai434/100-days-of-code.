95d561deb0e2f01e3f256210a057262c4b080526
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-01 22:06:02 +0530

    Update and rename day 02 greatest number .c to DAY 02 _A .C

---
+++ DAY 02 _A .C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    float length, breadth, area, perimeter;
+
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length);
+
+    printf("Enter the breadth of the rectangle: ");
+    scanf("%f", &breadth);
+
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    printf("\nArea of the rectangle = %.2f", area);
+    printf("\nPerimeter of the rectangle = %.2f", perimeter);
+
+    return 0;
+}
+
