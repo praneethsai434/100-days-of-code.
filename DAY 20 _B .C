f0847ff789f3d811e0e964751c7fd658cf87c142
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-01 23:16:19 +0530

    Create DAY 20 _B .C

---
+++ DAY 20 _B .C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char binary[50];
+    int i;
+
+    
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+   
+    for (i = 0; i < strlen(binary); i++) {
+        if (binary[i] == '0')
+            binary[i] = '1';
+        else if (binary[i] == '1')
+            binary[i] = '0';
+        else {
+            printf("Invalid binary number!\n");
+            return 0;
+        }
+    }
+
+  
+    printf("1's Complement: %s\n", binary);
+
+    return 0;
+}
