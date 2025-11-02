78c9e6e640de7f7c5898ab927873e24ce9720349
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-02 22:57:50 +0530

    Create DAY 26 _B .C

---
+++ DAY 26 _B .C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    printf("*\n\n");
+
+    for (i = 1; i <= 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for (i = 1; i <= 5; i++) {
+        printf("*\n");
+    }
+
+    return 0;
+}
