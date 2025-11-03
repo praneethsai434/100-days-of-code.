7b145e3d4a54d6eec640d40d0157c5035044f8ca
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-03 19:11:22 +0530

    Create DAY 37_B .C

---
+++ DAY 37_B .C
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int matrix[10][10], transpose[10][10];
+    int i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("\nOriginal Matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nTranspose of the Matrix:\n");
+    for (i = 0; i < cols; i++) {
+        for (j = 0; j < rows; j++) {
+            printf("%d\t", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
