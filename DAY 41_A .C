1e673faa9c19d430c9bd54ea1bd5c9bc8ab3c155
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-03 19:21:46 +0530

    Create DAY 41_A .C

---
+++ DAY 41_A .C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        count++;
+    }
+
+    printf("Number of characters in the string: %d\n", count);
+
+    return 0;
+}
