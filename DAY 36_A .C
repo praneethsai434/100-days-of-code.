9bdc0d983ef2d2b5dd29046c1a595363e026a3bc
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-03 19:06:08 +0530

    Create DAY 36_A .C

---
+++ DAY 36_A .C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int matrix[10][10];
+    int i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("\nThe entered matrix is:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
