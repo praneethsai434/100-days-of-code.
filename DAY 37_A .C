5b58e1543265a48505f69333f8bdb4005463db09
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-03 19:09:19 +0530

    Create DAY 37_A .C

---
+++ DAY 37_A .C
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    int matrix[10][10], rowSum[10];
+    int i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        rowSum[i] = 0;
+        for (j = 0; j < cols; j++) {
+            rowSum[i] += matrix[i][j];
+        }
+    }
+
+    printf("\nThe entered matrix is:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nSum of each row:\n");
+    for (i = 0; i < rows; i++) {
+        printf("Row %d = %d\n", i + 1, rowSum[i]);
+    }
+
+    return 0;
+}
