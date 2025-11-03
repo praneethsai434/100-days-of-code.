fccf980fa03bc00c457867ecfd65392f9c42a914
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-03 19:14:02 +0530

    Create DAY 38_B .C

---
+++ DAY 38_B .C
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    int matrix[10][10], transpose[10][10];
+    int isSymmetric = 1;
+
+    printf("Enter the order of the square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] != transpose[i][j]) {
+                isSymmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if (isSymmetric)
+        printf("The matrix is symmetric.\n");
+    else
+        printf("The matrix is not symmetric.\n");
+
+    return 0;
+}
