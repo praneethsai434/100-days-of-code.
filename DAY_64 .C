83ddcda2a8b5fbff3170715f071945f6a70923c2
praneethsai434 <varada.28521@stu.upes.ac.in>
2025-11-29 11:36:11 +0530

    Create DAY_64 .C

---
+++ DAY_64 .C
@@ -0,0 +1,39 @@
+//Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output.
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    if (k < 1 || k > n) {
+        printf("Invalid value of k.\n");
+        return 0;
+    }
+
+    // Sort array using simple selection sort
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        // Swap arr[i] and arr[min_idx]
+        int temp = arr[i];
+        arr[i] = arr[min_idx];
+        arr[min_idx] = temp;
+    }
+
+    // kth smallest element is at index k-1 after sorting
+    printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+
+    return 0;
+}
